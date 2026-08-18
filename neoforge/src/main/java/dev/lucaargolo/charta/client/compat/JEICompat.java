1c65cedd226654357d4f8e3bd24011e30bc513b3
Kaban4ik2023 <kaban4ikstudios@gmail.com>
2026-08-18 21:15:56 +0300

    Update JEICompat.java

--- neoforge/src/main/java/dev/lucaargolo/charta/client/compat/JEICompat.java
+++ neoforge/src/main/java/dev/lucaargolo/charta/client/compat/JEICompat.java
@@ -4,12 +4,10 @@ import dev.lucaargolo.charta.client.render.screen.GameScreen;
 import dev.lucaargolo.charta.common.ChartaMod;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.gui.handlers.IGuiContainerHandler;
+import mezz.jei.api.gui.handlers.IGuiProperties;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
-import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.resources.ResourceLocation;
-
-import java.util.List;
 
 @JeiPlugin
 public class JEICompat implements IModPlugin {
@@ -20,15 +18,44 @@ public class JEICompat implements IModPlugin {
     }
 
     @Override
+    @SuppressWarnings({"rawtypes", "unchecked"})
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
-        registration.addGenericGuiContainerHandler(
-                GameScreen.class,
-                new IGuiContainerHandler<GameScreen<?, ?>>() {
-                    @Override
-                    public List<Rect2i> getGuiExtraAreas(GameScreen<?, ?> screen) {
-                        return List.of(new Rect2i(0, 0, screen.width, screen.height));
-                    }
-                }
-        );
+        registration.addGuiScreenHandler(GameScreen.class, screen -> new IGuiProperties() {
+
+            @Override
+            public Class<? extends Screen> screenClass() {
+                return screen.getClass();
+            }
+
+            @Override
+            public int guiLeft() {
+                return 0;
+            }
+
+            @Override
+            public int guiTop() {
+                return 0;
+            }
+
+            @Override
+            public int guiXSize() {
+                return screen.width;
+            }
+
+            @Override
+            public int guiYSize() {
+                return screen.height;
+            }
+
+            @Override
+            public int screenWidth() {
+                return screen.width;
+            }
+
+            @Override
+            public int screenHeight() {
+                return screen.height;
+            }
+        });
     }
 }
